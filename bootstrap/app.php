d7c209c6121e0a90ff0cb8ead278e6cf8f7ca891
ben_abednego <ben_abednego@192.168.0.110>
2026-08-16 20:41:33 +0800

    fix: use native LARAVEL_STORAGE_PATH and stderr logging for Vercel serverless

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,14 +6,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->registered(function ($app) {
-        if ($storagePath = getenv('APP_STORAGE_PATH')) {
-            $app->useStoragePath($storagePath);
-        }
-        if (getenv('APP_BOOTSTRAP_PATH')) {
-            $app->useBootstrapPath(getenv('APP_BOOTSTRAP_PATH'));
-        }
-    })
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
